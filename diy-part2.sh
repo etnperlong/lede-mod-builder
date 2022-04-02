17ba09c4250f8861ffdcec46e2405b74cfdc9450
Mike Evans <etnperlong@gmail.com>
2022-04-02 19:08:15 +0100

    Revert path

--- diy-part2.sh
+++ diy-part2.sh
@@ -31,3 +31,6 @@ popd
 
 # Add Support for Asterisk Quectel dongle
 git clone https://github.com/etnperlong/openwrt-asterisk-chan-quectel.git feeds/package/telephony/net/openwrt-asterisk-chan-quectel
+
+# Install again
+./scripts/feeds install -a
